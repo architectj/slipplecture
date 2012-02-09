df7e9ecaaaaa771141ca0c418767dbb6b3088a7c
javajigi <javajigi@gmail.com>
2012-02-09 21:13:22 +0900

    Connection Pooling 기능 제공할 수 있도록 DataSource 기능 추가함.

--- day1/src/main/java/net/slipp/support/jdbc/ConnectionManager.java
+++ day1/src/main/java/net/slipp/support/jdbc/ConnectionManager.java
@@ -1,26 +1,39 @@
 package net.slipp.support.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
 
 public class ConnectionManager {
-	private static Logger logger = LoggerFactory.getLogger(ConnectionManager.class);
-	
-	public static Connection getConnection() {
-		String url = "jdbc:mysql://localhost:3306/";
-		String dbName = "lecture";
-		String driver = "com.mysql.jdbc.Driver";
-		String userName = "userid";
-		String password = "password";
-		try {
-			Class.forName(driver).newInstance();
-			return DriverManager.getConnection(url + dbName, userName, password);
-		} catch (Exception e) {
-			logger.error(e.getMessage());
-			return null;
-		}
-	}
+    public static Connection getConnection() throws SQLException {
+//        String url = "jdbc:mysql://localhost:3306/lecture";
+//        String driver = "com.mysql.jdbc.Driver";
+//        String username = "userid";
+//        String password = "password";
+//        try {
+//            Class.forName(driver).newInstance();
+//            return DriverManager.getConnection(url, username, password);
+//        } catch (Exception e) {
+//            logger.error(e.getMessage());
+//            return null;
+//        }
+        return getDataSource().getConnection();
+    }
+
+    public static DataSource getDataSource() {
+        String url = "jdbc:mysql://localhost:3306/lecture";
+        String driver = "com.mysql.jdbc.Driver";
+        String username = "userid";
+        String password = "password";
+        
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
 }
