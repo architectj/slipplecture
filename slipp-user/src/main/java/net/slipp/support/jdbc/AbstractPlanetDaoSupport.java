dec62e07241da850202463165bdd3cfe241c446e
Joseph Yang <Joseph Yang@JosephYang-PC>
2012-04-27 17:15:21 +0900

    데이터 초기화 작업

--- slipp-user/src/main/java/net/slipp/support/jdbc/AbstractPlanetDaoSupport.java
+++ slipp-user/src/main/java/net/slipp/support/jdbc/AbstractPlanetDaoSupport.java
@@ -4,17 +4,32 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 public abstract class AbstractPlanetDaoSupport implements InitializingBean {
 	private JdbcTemplate jdbcTemplate;
 	
 	@Autowired
 	private DataSource dataSource;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		jdbcTemplate = new JdbcTemplate(dataSource);
+		DatabasePopulatorUtils.execute(databasePopulator(), dataSource);
+	}
+	
+	private static DatabasePopulator databasePopulator() {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("user.sql"));
+		return populator;
 	}
 	
 	public JdbcTemplate getJdbcTemplate() {
